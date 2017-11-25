0b3b2243c7ecdcb993aa40d58a2ba9df5c685f05
Pasever <pas1034ever@gmail.com>
2017-11-24 21:01:53 -0500

    a.0.6

--- bamazon.sql
+++ bamazon.sql
@@ -7,13 +7,14 @@ CREATE TABLE products(
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(45) NOT NULL,
+  price INT(40) NOT NULL,
   stock_quantity VARCHAR(50),
   PRIMARY KEY (item_id)
 );
 
 
-INSERT INTO products (product_name, department_name, stock_quantity)
-      VALUES ("apple", "fruits", 100), ("banana", "fruits", 120), ("orange", "fruits", 75),
-             ("grape", "fruits", 123), ("pineapple", "fruits", 65), ("strawberry", "fruits", 90),
-             ("mango", "fruits", 214), ("peach", "fruits", 62), ("watermelon", "fruits", 999),
-             ("grapefruit", "fruits", 825), ("papaya", "fruits", 272), ("coconut", "fruits", 733);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+      VALUES ("apple", "fruits", 2.33, 100), ("banana", "fruits", 2.56, 120), ("orange", "fruits", 5.34, 75),
+             ("grape", "fruits", 1.78, 123), ("pineapple", "fruits", 4.77, 65), ("strawberry", "fruits", 2.55, 90),
+             ("mango", "fruits", 3.12, 214), ("peach", "fruits", 3.33, 62), ("watermelon", "fruits", 2,67, 999),
+             ("grapefruit", "fruits", 4.22, 825), ("papaya", "fruits", 4.10, 272), ("coconut", "fruits", 4.66, 733);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-var connection = mysql.createConnection({host: "localhost", port: 3306, user: "root", password: "root", database: "bamazon"});
+var connection = mysql.createConnection(
+  {
+      host: "localhost", 
+      port: 3306, 
+      user: "root", 
+      password: "root", 
+      database: "bamazon"
+});
 
 connection.connect(function(err) {
   if (err) 
     throw err;
-  console.log("connected as id " + connection.threadId);
+  
+  //console.log("connected as id " + connection.threadId);
   afterConnection();
 });
 
@@ -14,14 +22,22 @@ function afterConnection() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) 
       throw err;
-    console.log(res);
+    console.log("Welcome to BAMAZON!")
+    console.log("Items in stock:");
+    console.log("_________________________________________");
+    console.log("");
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " | " + res[i].product_name + " | $" + res[i].price + "ea" + " | Qty: " + res[i].stock_quantity);
+    }
+    console.log("_________________________________________");
+    console.log("");
+    //console.log(res);
     availability();
-    //connection.end();
   });
 }
 
 function availability() {
-  // prompt for info about the item being put up for auction
+
   inquirer.prompt([
     {
       name: "itemID",
@@ -38,29 +54,73 @@ function availability() {
       item_id: answer.itemID,
       item_qty: answer.itemQTY
     }, function(err, results) {
-      if (err) throw err;
-    
+      if (err) 
+        throw err;
       
-    
-        console.log("Pulling the information...");
-        console.log(results);
-        console.log(typeof parseInt(answer.itemID));
-        console.log(parseInt(answer.itemQTY));
-        let stock = false;
-        
-        for (var i = 0; i < results.length; i++) {
-          
-          if (results[i].item_id == answer.itemID && results[i].stock_quantity > answer.itemQTY) {
-            stock = true;
-            console.log("Yep, we have that in stock...");
-            console.log("Qty is enough to fulfill order...");
-          }
-        }      
-      
-      if (!stock) {
-        console.log("Insufficient quantity!");
-        //availability();        
-      }
+      checkAvailability(answer.itemID, answer.itemQTY);
+
     });
+  });
+}
+
+function checkAvailability(item, quantity) {
+  connection.query("SELECT price, stock_quantity, product_sales FROM products WHERE ?", {
+    item_id: item
+  }, function(err, result) {
+    if (err) 
+      throw err;
+    
+    if (quantity > result[0].stock_quantity) {
+      console.log("Insufficient Stock for this Item!");
+    } else {
+      var total = parseFloat(result[0].price * quantity).toFixed(2);
+      var newQty = result[0].stock_quantity - quantity;
+      console.log("");
+      console.log("Qty is enough to fulfill your order...");
+      console.log("Your total is going to be $" + total);
+      console.log("");
+
+      inquirer.prompt([
+        {
+          name: 'confirm',
+          type: 'confirm',
+          message: 'Would you like to place this order?'
+        }
+      ]).then(function(answer) {
+        if (answer.confirm) {
+          console.log("");
+          console.log('Thank you for your purchase!');
+          console.log("Your credit card will be charged $" + total);
+          console.log("");
+
+          // update database qty
+          //database.updateStock(id, newQty);
+          
+          // update sales qty
+          //database.addSale(department, total);
+
+          inquirer.prompt([
+            {
+              name: 'confirm',
+              type: 'confirm',
+              message: 'Return to home page?'
+            }
+          ]).then(function(answer) {
+            if (answer.confirm) {
+              availability();
+            } else {
+              console.log('Thank you for BAMAZONing!');
+              process.exit();
+            }
+          });
+        } else {
+          console.log("");
+          console.log('Returning to home page...');
+          console.log("");
+          availability();
+
+        }
+      });
+    }
   });
 }
